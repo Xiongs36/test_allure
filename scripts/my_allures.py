a985c8f8ca997f330cde28a8efd18922cf5fa52b
Xiongs36 <1037876630@qq.com>
2022-10-19 18:03:35 +0800

    s

---
+++ scripts/my_allures.py
@@ -0,0 +1,20 @@
+import os
+import sys
+import allure
+import pytest
+
+class MyAllures():
+    @allure.step('测试第一步1,True')
+    def test_01(self):
+        allure.attach('描述','断言成功')
+        assert True
+
+    @allure.step('测试第二步1，False')
+    @allure.testcase('http://www.baidu.com')
+    @allure.issue('http://www.baidu.com')
+    @allure.severity(allure.severity_level.BLOCKER)
+    def test_02(self):
+        assert False
+# if __name__ == '__main__':
+#     pytest.main()
+#     os.system("allure generate --clean ./temp -o ./report/")
